16cfac13e7b9362c9258a070d789e3050862b717
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-10-10 11:51:44 -0400

    commented/annotated routes

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
+// Importing necessary packages + necessary boilerplate
 const express = require("express"),
   bodyParser = require("body-parser"),
   app = express();
-
+app.use(bodyParser.json());
 let dbInteractions = require("./database");
 const PORT = 3000;
 
-app.use(bodyParser.json());
-
+// GET /todos returns a list of all the todos
 app.get("/todos", (req, res) => {
   let todos = dbInteractions.getTodos();
   res.status(200).json(todos);
 });
 
+// GET /todos/:id returns a specific todo by ID
+// @params:
+//     - "id" in the path
 app.get("/todos/:id", (req, res) => {
   let todo = dbInteractions.getTodo();
   res.status(200).json(todo);
 });
 
+// POST /todos creates a new todo and returns it
+// @params:
+//     - "message" in the body
 app.post("/todos", (req, res) => {
   let newTodo = dbInteractions.createTodo(req.body.message);
   res.status(200).json(newTodo);
 });
 
+// PUT /todos/:id updates the message of a todo and returns the todo
+// @params:
+//     - "id" in the path
+//     - "message" in the body
 app.put("/todos/:id", (req, res) => {
   let updatedTodo = dbInteractions.updateTodo(req.params.id, req.body.message);
   res.status(200).json(updatedTodo);
 });
 
+// DELETE /todos/:id deletes a todo and returns "success": true or false
+// @params:
+//     - "id" in the path
 app.delete("/todos/:id", (req, res) => {
   let result = dbInteractions.deleteTodo(req.params.id);
   res.status(200).json({
@@ -34,6 +47,7 @@ app.delete("/todos/:id", (req, res) => {
   });
 });
 
+// Initializes the server on a certain PORT
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
 });
