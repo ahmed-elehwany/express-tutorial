fae862b5249a14f3b210898344bdb773586278f4
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-10-20 19:02:31 -0400

    frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>EXPRESS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <Table />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Table from "./components/Table";
+
+export default {
+  name: "App",
+  components: {
+    Table
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
