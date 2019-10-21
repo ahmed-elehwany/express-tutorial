83e4487ddd41f540aa03f0a203c26ccf53ff1933
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-10-20 21:25:13 -0400

    removed solutions from sample app routes

--- app.js
+++ app.js
@@ -11,38 +11,21 @@ const PORT = 3000;
 
 // GET /todos returns a list of all the todos
 app.get("/todos", (req, res) => {
-  let todos = dbInteractions.getTodos();
-  res.status(200).json(todos);
+  // TODO: fill out functionality
 });
 
 // GET /todos/:id returns a specific todo by ID
 // @params:
 //     - "id" in the path
 app.get("/todos/:id", (req, res) => {
-  dbInteractions.getTodo(req.params.id)
-    .then((todo) => {
-      return res.status(200).json(todo);
-    })
-    .catch(error => {
-      return res.status(404).json({
-        msg: error
-      });
-    })
+  // TODO: fill out functionality
 });
 
 // POST /todos creates a new todo and returns it
 // @params:
 //     - "message" in the body
 app.post("/todos", (req, res) => {
-  dbInteractions.createTodo(req.body.message)
-    .then(newTodo => {
-      return res.status(200).json(newTodo);
-    })
-    .catch(error => {
-      return res.status(500).json({
-        msg: error
-      })
-    })
+  // TODO: fill out functionality
 });
 
 // PUT /todos/:id updates the message of a todo and returns the todo
@@ -50,34 +33,14 @@ app.post("/todos", (req, res) => {
 //     - "id" in the path
 //     - "message" in the body
 app.put("/todos/:id", (req, res) => {
-  dbInteractions.updateTodo(req.params.id, req.body.message)
-    .then(updatedTodo => {
-
-      return res.status(200).json(updatedTodo);
-    })
-    .catch(error => {
-      return res.status(404).json({
-        msg: error
-      })
-    })
+  // TODO: fill out functionality
 });
 
 // DELETE /todos/:id deletes a todo and returns "success": true or false
 // @params:
 //     - "id" in the path
 app.delete("/todos/:id", (req, res) => {
-  let result = dbInteractions.deleteTodo(req.params.id)
-    .then(result => {
-      res.status(200).json({
-        success: result
-      });
-    })
-    .catch(error => {
-      return res.status(404).json({
-        success: false,
-        msg: error
-      })
-    })
+  // TODO: fill out functionality
 });
 
 // Initializes the server on a certain PORT
